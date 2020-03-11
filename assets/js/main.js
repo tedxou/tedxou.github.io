08c227c0fbbc4dbe2b3655e845e3c6ac4228ce46
VISHWATEJ <vishwatej9991@gmail.com>
2020-03-11 21:37:05 +0530

    com

--- assets/js/main.js
+++ assets/js/main.js
@@ -66,6 +66,24 @@ btn.on('click', function(e) {
 });
 
 
+$(document).scroll(function() {
+  var scroll = $(this).scrollTop();
+  if (scroll >= 150) {
+    $("#popUp").css("margin-left", "-425px");
+    $("#plus").css("margin-left", "0px");
+  }
+});
+
+$("#plus").click(function() {
+  $("#popUp").css("margin-left", "0px");
+  $("#plus").css("margin-left", "-425px");
+});
+
+$("#close").click(function() {
+  $("#popUp").css("margin-left", "-425px");
+  $("#plus").css("margin-left", "0px");
+});
+
     /* ==============================================
 /*  PRE LOADING
   =============================================== */
