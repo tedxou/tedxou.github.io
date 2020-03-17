8ebcb1c4a705e6ab4b3114dc93f123f6170b1c30
VISHWATEJ <vishwatej9991@gmail.com>
2020-03-17 15:31:53 +0530

    l

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,9 +9,7 @@
  /* ------------------------------------- */
 /*   PRE LOADING                          */
 /*   WOW                                 */
-/*   sliders                      */
-/*    MAPS                               */
-/*   COUNTER JS              */
+
 
 const nodes = [].slice.call(document.querySelectorAll('li'), 0);
 const directions  = { 0: 'top', 1: 'right', 2: 'bottom', 3: 'left' };
